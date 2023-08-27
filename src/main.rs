7687ff58db5395da8459147bbde46e5a29fba70a
Kischy <kischy@gmx.de>
2023-08-27 16:03:49 +0200

    Initialized empty project

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let problem_number = 36;
+    let mut answer_p35 = 0;
+
+    println!(
+        "The answer to problem {} of project Euler is {}.",
+        problem_number, answer_p35
+    );
+}
